8c077378f18725c08572f995e302f119676e7464
veryjoh <jgs8115@gmail.com>
2019-11-11 20:45:41 -0300

    obs README.md

--- bin/server.js
+++ bin/server.js
@@ -1,56 +1,35 @@
-#!/usr/bin/env node
+'use strict';
 
-/**
- * Module dependencies.
- */
 
 var app = require('../app');
 var http = require('http');
 
-/**
- * Get port from environment and store in Express.
- */
 
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
 
 var server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
-    // named pipe
     return val;
   }
 
   if (port >= 0) {
-    // port number
     return port;
   }
 
   return false;
 }
-
-/**
- * Event listener for HTTP server "error" event.
- */
 
 function onError(error) {
   if (error.syscall !== 'listen') {
@@ -61,7 +40,6 @@ function onError(error) {
     ? 'Pipe ' + port
     : 'Port ' + port;
 
-  // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges');
@@ -86,6 +64,6 @@ function onListening() {
     ? 'pipe ' + addr
     : 'port ' + addr.port;
 
-    console.log('===>',bind);
-    
+  console.log('===>', bind);
+
 }
