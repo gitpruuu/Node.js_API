f0e4c27464395da15e9465237e8bf6df62804e51
veryjoh <jgs8115@gmail.com>
2019-11-12 08:38:55 -0300

    home route

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,7 +10,7 @@ const jwt = require('jsonwebtoken');
 
 exports.home = (req, res) => {
     res.status(200).send({
-        mensagem: "Hello, Heroku!!!;"
+        mensagem: "Hello, Heroku!!!"
     })
 }
 
